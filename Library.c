749fe105b7970e2da8edbf6c95c4834c591c3f5e
MickeyGT <MickeyGT@users.noreply.github.com>
2016-03-18 00:25:31 +0200

    Update Library.c

--- Library.c
+++ Library.c
@@ -1,10 +1,22 @@
 #include"Library.h"
 
-
-
-void merge_sort(int n, int vector[100000], int beg, int end)
+void merge(int vector[100000],int frst,int midl,int endg)
 {
+	int limit1, limit2;
+	limit1 = midl - frst + 1;
+	limit2 = endg - midl;
+	for()
+}
 
+void merge_sort(int beg, int end, int vector[100000])
+{
+	int mid;
+	if (beg < end)
+		mid = (beg + end) / 2;
+	merge(vector, beg, mid);
+	merge(vector, mid, end);
+	merge(vector, beg, mid, end);
+	
 }
 
 void readarray(int n, int vector[100000])
