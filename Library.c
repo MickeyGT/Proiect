e688250219982c11e672938f3290cef130b61ace
MickeyGT <MickeyGT@users.noreply.github.com>
2016-03-18 00:04:18 +0200

    Create Library.c

---
+++ Library.c
@@ -0,0 +1,23 @@
+#include"Library.h"
+
+
+
+void merge_sort(int n, int vector[100000], int beg, int end)
+{
+
+}
+
+void readarray(int n, int vector[100000])
+{
+	int i;
+	for (i = 1;i <= n;i++)
+		scanf("%d", vector[i]);
+}
+
+void printarray(int n, int vector[100000])
+{
+	int i;
+	for (i = 1;i <= n;i++)
+		printf("%d", vector[i]);
+
+}
