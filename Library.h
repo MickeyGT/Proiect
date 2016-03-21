1d30ff3c6b285e50a0165bf488e1a3c9c6fafbb3
MickeyGT <MickeyGT@users.noreply.github.com>
2016-03-21 20:33:10 +0200

    Update Library.h

--- Library.h
+++ Library.h
@@ -1,8 +1,11 @@
-#ifndef Library.h
-#define Library.h
+#ifndef Library
+#define Library
 
-void merge_sort();
+void MergeSort(float vector[100000], int beg, int end,float B[100000]);
 
+void MergeParts(float A[100000], int beg, int mid, int end, float B[100000]);
 
+void readarray(int n, float vector[100000]);
 
+void printarray(int n, float vector[100000]);
 #endif
