541678cabb71e390d6aff8c1ee7060c6bb79a4b0
MickeyGT <MickeyGT@users.noreply.github.com>
2016-03-21 20:32:55 +0200

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,11 +1,16 @@
 #include "Library.h"
 #include<stdio.h>
+#include<stdlib.h>
 
-
-
+float v[100000],v1[100000];
+int n;
 int main()
 {
-
-
-
+	scanf("%i", &n);
+	readarray(n, v);
+	printarray(n, v);
+	MergeSort(v, 1, n+1,v1);
+	printarray(n, v);
+	system("pause");
+	return 0;
 }
