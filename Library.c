be1e2074f3306ed982133158d63663e5050add17
MickeyGT <MickeyGT@users.noreply.github.com>
2016-03-21 20:32:42 +0200

    Update Library.c

--- Library.c
+++ Library.c
@@ -1,35 +1,52 @@
 #include"Library.h"
-
-void merge(int vector[100000],int frst,int midl,int endg)
+#include<stdio.h>
+#include<stdlib.h>
+void MergeSort(float vector[100000],int beg,int end,float B[100000])
 {
-	int limit1, limit2;
-	limit1 = midl - frst + 1;
-	limit2 = endg - midl;
-	for()
+	if (end - beg <= 1)                      
+		return;                                											
+	int mid = (end + beg) / 2;              
+	MergeSort(vector, beg, mid,B);
+	MergeSort(vector, mid, end,B);
+	MergeParts(vector, beg, mid, end,B);
+	for (int k = beg;k < end;k++)
+		vector[k] = B[k];							          
 }
 
-void merge_sort(int beg, int end, int vector[100000])
+void MergeParts(float A[100000],int beg,int mid,int end,float B[100000])
 {
-	int mid;
-	if (beg < end)
-		mid = (beg + end) / 2;
-	merge(vector, beg, mid);
-	merge(vector, mid, end);
-	merge(vector, beg, mid, end);
-	
+	int ind1, ind2;
+	//float B[100000];
+	ind1 = beg;
+	ind2 = mid;
+	for (int k = beg; k < end; k++)
+	{
+		if ((ind1 < mid) && ((ind2 >= end)|| (A[ind1] <= A[ind2])))
+		{
+			B[k] = A[ind1];
+			ind1 = ind1 + 1;
+		}
+		else 
+		{
+			B[k] = A[ind2];
+			ind2 = ind2 + 1;
+		}
+	}
+
 }
 
-void readarray(int n, int vector[100000])
+void readarray(int n, float vector[100000])
 {
 	int i;
 	for (i = 1;i <= n;i++)
-		scanf("%d", vector[i]);
+		scanf("%f", &vector[i]);
 }
 
-void printarray(int n, int vector[100000])
+void printarray(int n, float vector[100000])
 {
 	int i;
 	for (i = 1;i <= n;i++)
-		printf("%d", vector[i]);
+		printf("%f ", vector[i]);
+	printf("\n");
 
 }
