c030bad373b2461945cccf6ed6d00e5c747c47a6
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2016-04-01 01:05:41 +0300

    Update Source.c
    HeapSort Add

--- Source.c
+++ Source.c
@@ -3,14 +3,22 @@
 #include<stdlib.h>
 
 float v[100000],v1[100000];
-int n;
+int n,el;
 int main()
 {
+	printf("N=");
 	scanf("%i", &n);
-	readarray(n, v);
-	printarray(n, v);
-	MergeSort(v, 1, n+1,v1);
-	printarray(n, v);
+	printf("Print %d elements:\n", n);
+	struct node *root;
+	root = (struct node*) malloc(sizeof(struct node));
+	root->left = NULL;
+	root->right = NULL;
+	scanf("%d", &el);
+	root->data = el;
+	ConstructHeap(n,root);
+	printf("The sorted elements from Heap:\n");
+	HeapSort(root);
+	printf("\n");
 	system("pause");
 	return 0;
 }
