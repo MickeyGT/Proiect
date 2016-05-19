ba741f3ea87a47cb6b2f207be5f95c2a38d05793
Arkin1 <stoicaastefan@gmail.com>
2016-05-19 23:13:04 +0300

    Add files via upload

--- set.h
+++ set.h
@@ -2,7 +2,7 @@
 #define SET_H
 
 /*
-  The set is represented using a red-black tree which guarantees deletion,insertion and searching in
+  The set is represented using a red-black tree which guarantees insertion and searching in
   O(log(n)).Elements are unique.
 */
 
@@ -29,8 +29,6 @@ void set_init(set_node **set);
 //Inserting an element in the set based on a custom defined comparation function
 void set_insert(set_node **set, void *elem, const int size, int(*cmp)(const void*, const void*));
 
-//Deleting an element in the set based on a custom defined comparation function
-void set_delete(set_node *set, void *elem, const int size, int(*cmp)(const void*, const void*));
 
 //Searching an element in the set based on a custom defined comparation function
 int set_search(set_node *set, void *elem, int(*cmp)(const void*, const void*));
